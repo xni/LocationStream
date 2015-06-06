d9e6d7ce2eeb5607a40f8604035734a5976ac05e
Konstantin Nikitin <stromsund@yandex-team.ru>
2015-06-06 20:01:10 +0300

    LibEV

--- server/chat_manager.cpp
+++ server/chat_manager.cpp
@@ -2,8 +2,41 @@
 // Created by Konstantin Nikitin on 06.06.15.
 //
 
+#include <signal.h>
+#include <stdio.h>
+
 #include <hiredis.h>
+#include <async.h>
+#include <adapters/libev.h>
+
+void connectCallback(const redisAsyncContext *c, int status) {
+    if (status != REDIS_OK) {
+        printf("Error: %s\n", c->errstr);
+        return;
+    }
+    printf("Connected...\n");
+}
+
+void disconnectCallback(const redisAsyncContext *c, int status) {
+    if (status != REDIS_OK) {
+        printf("Error: %s\n", c->errstr);
+        return;
+    }
+    printf("Disconnected...\n");
+}
 
 int main() {
+    signal(SIGPIPE, SIG_IGN);
+
+    redisAsyncContext *c = redisAsyncConnect("127.0.0.1", 6379);
+    if (c->err) {
+        /* Let *c leak for now... */
+        printf("Error: %s\n", c->errstr);
+        return 1;
+    }
+
+    redisLibevAttach(EV_DEFAULT_ c);
+    redisAsyncSetConnectCallback(c,connectCallback);
+    redisAsyncSetDisconnectCallback(c,disconnectCallback);
     return 0;
 }
